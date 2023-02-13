d1d9d382be1044881f2bcd5faba3a3256f9a7235
Sabry Awad <dr.sabry1997@gmail.com>
2023-02-13 11:32:59 +0200

    more organized way

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
 use std::fs::File;
 use std::io::{self, Write};
 
-use chrono::Datelike;
+use chrono::{Datelike, Weekday};
+
+const VERTICAL_LINE: &str = "|";
+const EOL: &str = "\n";
+const SPACE: &str = " ";
+const WEEK_DAYS: &str =
+    "...Sunday.....Monday....Tuesday...Wednesday...Thursday....Friday....Saturday..\n";
+const WEEK_SEPARATOR: &str =
+    "+----------+----------+----------+----------+----------+----------+----------+\n";
+const BLANK_ROW: &str =
+    "|          |          |          |          |          |          |          |\n";
+
+fn read_year() -> i32 {
+    let mut year = String::new();
+    println!("Enter the year: ");
+    io::stdin().read_line(&mut year).unwrap();
+    year.trim().parse().unwrap()
+}
+
+fn read_month() -> u32 {
+    let mut month = String::new();
+    println!("Enter the month: ");
+    io::stdin().read_line(&mut month).unwrap();
+    month.trim().parse().unwrap()
+}
 
 fn main() {
     let year = read_year();
@@ -11,7 +35,7 @@ fn main() {
 
     calendar.write_to_file();
 
-    println!("{}", calendar.cal_text);
+    // println!("{}", calendar.cal_text);
 }
 
 struct Month;
@@ -36,74 +60,85 @@ impl Month {
     }
 }
 
+struct Day {
+    number: u32,
+    // week_day: Weekday,
+}
+
+struct Week {
+    days: Vec<Day>,
+}
+
+impl Week {
+    fn to_string(&self) -> String {
+        let mut day_number_row = String::new();
+
+        for day in &self.days {
+            day_number_row.push_str(VERTICAL_LINE);
+            day_number_row.push_str(&format!("{:>2}", day.number));
+            day_number_row.push_str(&SPACE.repeat(8));
+        }
+
+        day_number_row.push_str(VERTICAL_LINE);
+        day_number_row.push_str(EOL);
+
+        for _ in 0..3 {
+            let blank_row = String::from(BLANK_ROW);
+            day_number_row.push_str(&blank_row);
+        }
+        return day_number_row;
+    }
+}
+
 struct Calendar {
     year: i32,
     month: u32,
-    cal_text: String,
+    weeks: Vec<Week>,
 }
 
 impl Calendar {
     fn new(year: i32, month: u32) -> Calendar {
-        let cal_text = Calendar::make_calendar(year, month);
-
-        Calendar {
-            year,
-            month,
-            cal_text,
-        }
-    }
-
-    fn make_calendar(year: i32, month: u32) -> String {
-        const VERTICAL_LINE: &str = "|";
-        const EOL: &str = "\n";
-        const SPACE: &str = " ";
-        const WEEK_DAYS: &str =
-            "...Sunday.....Monday....Tuesday...Wednesday...Thursday....Friday....Saturday..\n";
-        const WEEK_SEPARATOR: &str =
-            "+----------+----------+----------+----------+----------+----------+----------+\n";
-        const BLANK_ROW: &str =
-            "|          |          |          |          |          |          |          |\n";
-
-        let mut cal_text = String::new();
-
-        cal_text.push_str(&format!(
-            "{} {} {}",
-            SPACE.repeat(34),
-            Month::name(month),
-            year
-        ));
-        cal_text.push_str(EOL);
-        cal_text.push_str(WEEK_DAYS);
-
         let mut current_date = chrono::NaiveDate::from_ymd_opt(year, month, 1).unwrap();
+        let mut weeks = Vec::new();
 
         while current_date.weekday() != chrono::Weekday::Sun {
             current_date = current_date - chrono::Duration::days(1);
         }
 
         loop {
-            cal_text.push_str(WEEK_SEPARATOR);
-
-            let mut day_number_row = String::new();
-
+            let mut days = Vec::new();
             for _ in 0..7 {
-                day_number_row.push_str(VERTICAL_LINE);
-                day_number_row.push_str(&format!("{:>2}", current_date.day()));
-                day_number_row.push_str(&SPACE.repeat(8));
+                days.push(Day {
+                    number: current_date.day(),
+                });
                 current_date = current_date + chrono::Duration::days(1);
             }
 
-            day_number_row.push_str(VERTICAL_LINE);
-            day_number_row.push_str(EOL);
-            cal_text.push_str(&day_number_row);
-
-            for _ in 0..3 {
-                cal_text.push_str(BLANK_ROW);
-            }
+            weeks.push(Week { days });
 
             if current_date.month() != month {
                 break;
             }
+        }
+
+        Calendar { year, month, weeks }
+    }
+
+    fn to_string(&self) -> String {
+        let mut cal_text = String::new();
+
+        cal_text.push_str(&format!(
+            "{} {} {}",
+            SPACE.repeat(34),
+            Month::name(self.month),
+            self.year
+        ));
+        cal_text.push_str(EOL);
+        cal_text.push_str(WEEK_DAYS);
+
+        for week in &self.weeks {
+            cal_text.push_str(WEEK_SEPARATOR);
+            cal_text.push_str(&week.to_string());
         }
 
         cal_text.push_str(WEEK_SEPARATOR);
@@ -114,20 +149,6 @@ impl Calendar {
     fn write_to_file(&self) {
         let file_name = format!("calendar_{}_{}.txt", self.year, self.month);
         let mut file = File::create(file_name).unwrap();
-        file.write_all(self.cal_text.as_bytes()).unwrap();
+        file.write_all(self.to_string().as_bytes()).unwrap();
     }
-}
-
-fn read_year() -> i32 {
-    let mut year = String::new();
-    println!("Enter the year: ");
-    io::stdin().read_line(&mut year).unwrap();
-    year.trim().parse().unwrap()
-}
-
-fn read_month() -> u32 {
-    let mut month = String::new();
-    println!("Enter the month: ");
-    io::stdin().read_line(&mut month).unwrap();
-    month.trim().parse().unwrap()
 }
